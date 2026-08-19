40de5e17e71f5668a96df4faf05b8b30a964ae5f
Jorge Fernandez Martinez <166308528+Jorx8819@users.noreply.github.com>
2026-08-19 21:25:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ function showMainHub() {
         
         <div class="hub-grid">
           <div class="hub-card" onclick="selectHubSection('agents')">
-            <div class="hub-card-bg" style="background-image: url('https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png');"></div>
+            <div class="hub-card-bg"></div>
             <div class="hub-card-content">
               <span class="hub-badge">ROLES Y PODERES</span>
               <h3>Agentes</h3>
@@ -192,7 +192,7 @@ function showMainHub() {
           </div>
 
           <div class="hub-card" onclick="selectHubSection('weapons')">
-            <div class="hub-card-bg" style="background-image: url('https://media.valorant-api.com/weapons/ee8e8d15-496b-07ac-e5f6-8fae5d4c7b1a/displayicon.png');"></div>
+            <div class="hub-card-bg"></div>
             <div class="hub-card-content">
               <span class="hub-badge">ARSENAL</span>
               <h3>Armas y Skins</h3>
@@ -201,7 +201,7 @@ function showMainHub() {
           </div>
 
           <div class="hub-card" onclick="selectHubSection('maps')">
-            <div class="hub-card-bg" style="background-image: url('https://media.valorant-api.com/maps/ee613f1f-4d9a-418e-a4cf-3428383e206f/splash.png');"></div>
+            <div class="hub-card-bg"></div>
             <div class="hub-card-content">
               <span class="hub-badge">UBICACIONES</span>
               <h3>Mapas</h3>
@@ -210,7 +210,7 @@ function showMainHub() {
           </div>
 
           <div class="hub-card" onclick="selectHubSection('playercards')">
-            <div class="hub-card-bg" style="background-image: url('https://media.valorant-api.com/playercards/9fb53414-464e-f332-dfc0-83af0ca8d962/largeart.png');"></div>
+            <div class="hub-card-bg"></div>
             <div class="hub-card-content">
               <span class="hub-badge">COLECCIONABLES</span>
               <h3>Tarjetas</h3>
@@ -219,7 +219,7 @@ function showMainHub() {
           </div>
 
           <div class="hub-card" onclick="selectHubSection('sprays')">
-            <div class="hub-card-bg" style="background-image: url('https://media.valorant-api.com/sprays/d7285623-455b-4395-885e-99f6b9bc28fb/displayicon.png');"></div>
+            <div class="hub-card-bg"></div>
             <div class="hub-card-content">
               <span class="hub-badge">EXPRESIÓN</span>
               <h3>Graffitis</h3>
@@ -228,7 +228,7 @@ function showMainHub() {
           </div>
 
           <div class="hub-card" onclick="selectHubSection('buddies')">
-            <div class="hub-card-bg" style="background-image: url('https://media.valorant-api.com/buddies/12a87bc3-4889-f538-2d33-bc8efd18be02/displayicon.png');"></div>
+            <div class="hub-card-bg"></div>
             <div class="hub-card-content">
               <span class="hub-badge">ACCESORIOS</span>
               <h3>Llaveros</h3>
